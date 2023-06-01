3b8cecaa7ffff901b816267cfa5fd3fcff9e4310
DaNussi <daniel.nussbaum@htlstp.at>
2023-06-01 15:56:57 +0200

    Better Inventory in TestBlockEntity.java

--- src/main/java/net/nussi/dedicated_applied_energistics/blockentities/TestBlockEntity.java
+++ src/main/java/net/nussi/dedicated_applied_energistics/blockentities/TestBlockEntity.java
@@ -1,6 +1,7 @@
 package net.nussi.dedicated_applied_energistics.blockentities;
 
 import appeng.api.config.AccessRestriction;
+import appeng.api.inventories.BaseInternalInventory;
 import appeng.api.inventories.InternalInventory;
 import appeng.api.networking.IInWorldGridNodeHost;
 import appeng.api.storage.IStorageMounts;
@@ -17,6 +18,7 @@ import appeng.me.cells.CreativeCellHandler;
 import appeng.me.storage.MEInventoryHandler;
 import appeng.me.storage.NullInventory;
 import appeng.parts.storagebus.StorageBusPart;
+import appeng.util.inv.AppEngInternalInventory;
 import com.mojang.logging.LogUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -25,10 +27,12 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.nussi.dedicated_applied_energistics.init.BlockEntityTypeInit;
 import org.slf4j.Logger;
 
-public class TestBlockEntity extends AENetworkInvBlockEntity implements IStorageProvider {
+import java.util.HashMap;
+
+public class TestBlockEntity extends AENetworkInvBlockEntity {
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    private AppEngCellInventory inv = new AppEngCellInventory(this, 1);
+    private DedicatedInternalInventory inv = new DedicatedInternalInventory();
 
     public TestBlockEntity(BlockPos pos, BlockState blockState) {
         super(BlockEntityTypeInit.TEST_ENTITY_TYPE.get(), pos, blockState);
@@ -44,38 +48,31 @@ public class TestBlockEntity extends AENetworkInvBlockEntity implements IStorage
         LOGGER.info("onChangeInventory " + slot + " " + inv.toString());
     }
 
-    @Override
-    public void mountInventories(IStorageMounts storageMounts) {
 
-        StorageCell storageCell = BasicCellHandler.INSTANCE.getCellInventory(AEItems.ITEM_CELL_256K.stack(), null);
-        inv.setHandler(0, storageCell);
-        StorageBusInventory handler = new StorageBusInventory(storageCell);
-        storageMounts.mount(handler);
-    }
+    public static class DedicatedInternalInventory extends BaseInternalInventory {
 
-    @Override
-    public AECableType getCableConnectionType(Direction dir) {
-        return AECableType.SMART;
-    }
+        HashMap<Integer, ItemStack> inventory = new HashMap<>();
 
-    static class StorageBusInventory extends MEInventoryHandler {
-        public StorageBusInventory(MEStorage inventory) {
-            super(inventory);
+        public DedicatedInternalInventory() {
+
         }
 
         @Override
-        protected MEStorage getDelegate() {
-            return super.getDelegate();
+        public int size() {
+            return inventory.size() + 1;
         }
 
         @Override
-        protected void setDelegate(MEStorage delegate) {
-            super.setDelegate(delegate);
+        public ItemStack getStackInSlot(int slotIndex) {
+            ItemStack itemStack = inventory.get(slotIndex);
+            if(itemStack == null) itemStack = ItemStack.EMPTY;
+            return itemStack;
         }
 
-        public void setAccessRestriction(AccessRestriction setting) {
-            setAllowExtraction(setting.isAllowExtraction());
-            setAllowInsertion(setting.isAllowInsertion());
+        @Override
+        public void setItemDirect(int slotIndex, ItemStack stack) {
+            inventory.put(slotIndex, stack);
         }
     }
+
 }
