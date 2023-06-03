f4bfab1d163125eefd3396bf377a64ecfc7e3f9e
DaNussi <daniel.nussbaum@htlstp.at>
2023-06-03 18:00:15 +0200

    Everything is working again nur ohne Redis...

--- src/main/java/net/nussi/dedicated_applied_energistics/blockentities/InterDimensionalInterfaceBlockEntity.java
+++ src/main/java/net/nussi/dedicated_applied_energistics/blockentities/InterDimensionalInterfaceBlockEntity.java
@@ -1,55 +1,106 @@
 package net.nussi.dedicated_applied_energistics.blockentities;
 
+import appeng.api.inventories.BaseInternalInventory;
+import appeng.api.inventories.InternalInventory;
+import appeng.blockentity.AEBaseInvBlockEntity;
+import com.google.common.base.Preconditions;
 import com.mojang.logging.LogUtils;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.ForgeCapabilities;
-import net.minecraftforge.common.util.LazyOptional;
-import net.nussi.dedicated_applied_energistics.capabilities.InterDimensionalInterfaceCapability;
 import net.nussi.dedicated_applied_energistics.init.BlockEntityTypeInit;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 
-public class InterDimensionalInterfaceBlockEntity extends BlockEntity {
-    private final InterDimensionalInterfaceCapability itemHandler = new InterDimensionalInterfaceCapability();
-    private final LazyOptional<InterDimensionalInterfaceCapability> lazyItemHandler = LazyOptional.of(() -> this.itemHandler);
+import java.util.HashMap;
 
+public class InterDimensionalInterfaceBlockEntity extends AEBaseInvBlockEntity {
     private static final Logger LOGGER = LogUtils.getLogger();
+    private final InternalInventory internalInventory = new DedicatedInternalInventory();
 
     public InterDimensionalInterfaceBlockEntity(BlockPos pos, BlockState state) {
         super(BlockEntityTypeInit.INTER_DIMENSIONAL_INTERFACE_ENTITY_TYPE.get(), pos, state);
     }
 
     @Override
-    public void load(CompoundTag nbt) {
-        itemHandler.deserializeNBT(nbt.getCompound("inventory"));
-        super.load(nbt);
+    public InternalInventory getInternalInventory() {
+        return internalInventory;
     }
 
     @Override
-    public void saveAdditional(CompoundTag nbt) {
-        nbt.put("inventory", itemHandler.serializeNBT());
-        super.saveAdditional(nbt);
+    public void onChangeInventory(InternalInventory inv, int slot) {
+        LOGGER.info("onChangeInventory " + slot + " " + inv.getSlotInv(slot));
     }
 
-    @Override
-    public <T> @NotNull LazyOptional<T> getCapability(@NotNull Capability<T> capability, Direction direction) {
-        if (capability == ForgeCapabilities.ITEM_HANDLER) {
-            return this.lazyItemHandler.cast();
+
+    public static class DedicatedInternalInventory extends BaseInternalInventory {
+        HashMap<Integer, ItemStack> internalInventory = new HashMap<>();
+
+        @Override
+        public int size() {
+            return internalInventory.size() + 1;
         }
-        return super.getCapability(capability, direction);
+
+        @Override
+        public ItemStack getStackInSlot(int slotIndex) {
+            ItemStack out = internalInventory.get(slotIndex);
+            if(out == null) return ItemStack.EMPTY;
+            return out;
+        }
+
+        @Override
+        public void setItemDirect(int slotIndex, ItemStack stack) {
+            if(stack.getCount() == 0) {
+                internalInventory.remove(slotIndex);
+                return;
+            }
+            internalInventory.put(slotIndex, stack);
+            LOGGER.info("Stack: " + slotIndex + " " + stack.getItem() + " x " + stack.getCount());
+        }
+
+
+        /**
+         * Simular to the Original Method but without the maximum slot size check
+         * @param slot
+         * @param stack
+         * @param simulate
+         * @return
+         */
+        @Override
+        public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
+            Preconditions.checkArgument(slot >= 0 && slot < size(), "slot out of range");
+
+            if (stack.isEmpty() || !isItemValid(slot, stack)) {
+                return stack;
+            }
+
+            var inSlot = getStackInSlot(slot);
+            if (!inSlot.isEmpty() && !ItemStack.isSameItemSameTags(inSlot, stack)) {
+                return stack;
+            }
+
+            // Calculate how much free space there is in the targeted slot, considering
+            // an item-dependent maximum stack size, as well as a potential slot-based limit
+            int maxSpace = Integer.MAX_VALUE;                                               // Nussi Edit: Modified to not Check for maxSpace!
+            int freeSpace = maxSpace - inSlot.getCount();
+            if (freeSpace <= 0) {
+                return stack;
+            }
+
+            var insertAmount = Math.min(stack.getCount(), freeSpace);
+            if (!simulate) {
+                var newItem = inSlot.isEmpty() ? stack.copy() : inSlot.copy();
+                newItem.setCount(inSlot.getCount() + insertAmount);
+                setItemDirect(slot, newItem);
+            }
+
+            if (freeSpace >= stack.getCount()) {
+                return ItemStack.EMPTY;
+            } else {
+                var r = stack.copy();
+                r.shrink(insertAmount);
+                return r;
+            }
+        }
     }
-
-    @Override
-    public void invalidateCaps() {
-        super.invalidateCaps();
-        this.lazyItemHandler.invalidate();
-    }
-
-
 
 }
