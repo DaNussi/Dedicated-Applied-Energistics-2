2958c54530e3bb33c5db5d3bd9aef4d07e69bb64
DaNussi <daniel.nussbaum@htlstp.at>
2023-06-06 13:52:18 +0200

    Inventory Working properly Scho wieda ka redis :C

--- src/main/java/net/nussi/dedicated_applied_energistics/init/ItemInit.java
+++ src/main/java/net/nussi/dedicated_applied_energistics/init/ItemInit.java
@@ -8,6 +8,7 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import net.nussi.dedicated_applied_energistics.blockentities.InterDimensionalInterfaceBlockEntity;
 import net.nussi.dedicated_applied_energistics.items.InterDimensionalStorageCell;
 
 import static net.nussi.dedicated_applied_energistics.DedicatedAppliedEnegistics.MODID;
